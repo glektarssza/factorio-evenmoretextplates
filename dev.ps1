da2c2d12e0f3a0b30a8635c97b68cafc398db4a1
G'lek Tarssza <glek@glektarssza.com>
2025-05-18 14:21:32 -0600

    Fixed some path issues.

--- dev.ps1
+++ dev.ps1
@@ -10,15 +10,15 @@ if (-not "$local:factorioPath") {
     $local:factorioPath = "$env:FACTORIO_PATH";
 }
 
-if (-not (Test-Path -PathType Container -Path "$local:factorioPath\mods")) {
+if (-not (Test-Path -PathType Container -Path "$local:factorioPath/mods")) {
     New-Item -Type Directory -Name "mods" -Path "$local:factorioPath";
 }
 
-Write-Output "Symbolic linking `"$local:modName`" v$local:modVersion to `"$local:factorioPath\mods\$($local:modName)`"";
+Write-Output "Symbolic linking `"$local:modName`" v$local:modVersion to `"$local:factorioPath/mods/$local:modName`"";
 
-if (Test-Path -Path "$local:factorioPath/mods/$($local:modName)") {
-    Write-Warning "`"$local:factorioPath\mods\$($local:modName)`" already exists, attempting to delete..."
-    (Get-Item -Path "$local:factorioPath/mods/$($local:modName)").Delete();
+if (Test-Path -Path "$local:factorioPath/mods/$local:modName") {
+    Write-Warning "`"$local:factorioPath/mods/$local:modName`" already exists, attempting to delete..."
+    (Get-Item -Path "$local:factorioPath/mods/$local:modName").Delete();
 }
 
-return New-Item -Force -Type SymbolicLink -Target "$PSScriptRoot/$modName" "$local:factorioPath/mods/$($local:modName)";
+return New-Item -Force -Type SymbolicLink -Target "$PSScriptRoot/$local:modName" "$local:factorioPath/mods/$local:modName";
